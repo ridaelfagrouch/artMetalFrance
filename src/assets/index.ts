9c1aaf0eced908c651f5042556a2ab93b5d14cfc
ridaelfagrouch <relfagrouch@gear9.ma>
2024-09-16 21:52:30 +0100

    .

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,23 +1,23 @@
-import Logo from "../../public/assets/icons/Logo.png";
+import Logo from "./icons/Logo.png";
 
-import BodyHeaderImage1 from "../../public/assets/images/bodyHeader/bodyHeaderImage1.jpg";
-import BodyHeaderImage2 from "../../public/assets/images/bodyHeader/bodyHeaderImage2.jpg";
-import BodyHeaderImage3 from "../../public/assets/images/bodyHeader/bodyHeaderImage3.jpg";
-import BodyHeaderImage4 from "../../public/assets/images/bodyHeader/bodyHeaderImage4.jpg";
-import BodyHeaderImage5 from "../../public/assets/images/bodyHeader/bodyHeaderImage5.jpg";
-import BodyHeaderImage6 from "../../public/assets/images/bodyHeader/bodyHeaderImage6.jpg";
+import BodyHeaderImage1 from "./images/bodyHeader/bodyHeaderImage1.jpg";
+import BodyHeaderImage2 from "./images/bodyHeader/bodyHeaderImage2.jpg";
+import BodyHeaderImage3 from "./images/bodyHeader/bodyHeaderImage3.jpg";
+import BodyHeaderImage4 from "./images/bodyHeader/bodyHeaderImage4.jpg";
+import BodyHeaderImage5 from "./images/bodyHeader/bodyHeaderImage5.jpg";
+import BodyHeaderImage6 from "./images/bodyHeader/bodyHeaderImage6.jpg";
 
-import AboutImage1 from "../../public/assets/images/about/aboutImage1.jpg";
-import AboutImage2 from "../../public/assets/images/about/aboutImage2.jpg";
+import AboutImage1 from "./images/about/aboutImage1.jpg";
+import AboutImage2 from "./images/about/aboutImage2.jpg";
 
-import BodySlider1 from "../../public/assets/images/bodySlider/bodySlider1.jpg";
-import BodySlider2 from "../../public/assets/images/bodySlider/bodySlider2.jpg";
-import BodySlider3 from "../../public/assets/images/bodySlider/bodySlider3.jpg";
-import BodySlider4 from "../../public/assets/images/bodySlider/bodySlider4.jpg";
-import BodySlider5 from "../../public/assets/images/bodySlider/bodySlider5.jpg";
-import BodySlider6 from "../../public/assets/images/bodySlider/bodySlider6.jpg";
-import BodySlider7 from "../../public/assets/images/bodySlider/bodySlider7.jpg";
-import BodySlider8 from "../../public/assets/images/bodySlider/bodySlider8.jpg";
+import BodySlider1 from "./images/bodySlider/bodySlider1.jpg";
+import BodySlider2 from "./images/bodySlider/bodySlider2.jpg";
+import BodySlider3 from "./images/bodySlider/bodySlider3.jpg";
+import BodySlider4 from "./images/bodySlider/bodySlider4.jpg";
+import BodySlider5 from "./images/bodySlider/bodySlider5.jpg";
+import BodySlider6 from "./images/bodySlider/bodySlider6.jpg";
+import BodySlider7 from "./images/bodySlider/bodySlider7.jpg";
+import BodySlider8 from "./images/bodySlider/bodySlider8.jpg";
 
 export {
   Logo,
