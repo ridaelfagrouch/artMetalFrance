072e507c940f3af09a8f00608f8878eb590fde9a
ridaelfagrouch <elfagrouchrida21@gmail.com>
2024-10-18 16:30:35 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -34,14 +34,14 @@ function App() {
   const sections = [
     { id: "body-header", Component: BodyHeader, className: "" },
     { id: "service-slider", Component: Serviceslider, className: "" },
-    // { id: "about", Component: About, className: classStyle },
-    // { id: "expertises", Component: Expertises, className: "" },
-    // { id: "partners", Component: Partners, className: classStyle },
-    // { id: "realisations", Component: Realisations, className: "" },
-    // { id: "questions", Component: Questions, className: classStyle },
-    // { id: "contact", Component: Contact, className: "" },
-    // { id: "quote", Component: Quote, className: "" },
-    // { id: "footer", Component: Footer, className: classStyle },
+    { id: "about", Component: About, className: classStyle },
+    { id: "expertises", Component: Expertises, className: "" },
+    { id: "partners", Component: Partners, className: classStyle },
+    { id: "realisations", Component: Realisations, className: "" },
+    { id: "questions", Component: Questions, className: classStyle },
+    { id: "contact", Component: Contact, className: "" },
+    { id: "quote", Component: Quote, className: "" },
+    { id: "footer", Component: Footer, className: classStyle },
   ];
 
   return (
